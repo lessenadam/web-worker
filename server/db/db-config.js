938bc114b670fec32af821d2e371d25e3352e72c
Adam Lessen <lessena@gmail.com>
2016-08-14 16:40:08 -0700

    Init the mongodb / mongoose database

---
+++ server/db/db-config.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/web-worker');
+
+var websiteSchema = mongoose.Schema({
+  shortUrl: {
+    type:String, required:true
+  },
+  fullUrl: {
+    type:String, required:true
+  },
+  htmlContent: {
+    type:String, required:true
+  },
+});
+
+module.exports = mongoose.model('Website', websiteSchema);
